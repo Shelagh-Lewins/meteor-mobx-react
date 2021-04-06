6f7877db2c5d56271e9ba03ae260117ffa1be341
Shelagh Lewins <shelagh_lewins@hotmail.com>
2021-04-06 19:19:09 +0100

    added return type

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Hello from './Hello.tsx';
 import Info from './Info.tsx';
 
-export default function App() {
-	return (
-		<div>
-			<h1>Welcome to Meteor!</h1>
-			<Hello />
-			<Info />
-		</div>
-	);
-}
+const App: React.FunctionComponent = () => (
+	<div>
+		<h1>Welcome to Meteor!</h1>
+		<Hello />
+		<Info />
+	</div>
+);
+
+export default App;
