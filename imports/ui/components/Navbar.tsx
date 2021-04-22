525b503f395f7f1971925d8b2106ced932eef53e
Shelagh Lewins <shelagh_lewins@hotmail.com>
2021-04-22 18:12:44 +0100

    first steps to hamburger menu for mobile

--- imports/ui/components/Navbar.tsx
+++ imports/ui/components/Navbar.tsx
@@ -4,20 +4,29 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
 	'root': {
 		'flexGrow': 1,
 	},
 	'toolbar': {
-		'display': 'block',
+		'display': 'none',
 		[theme.breakpoints.up('md')]: {
 			'display': 'flex',
 			'flexGrow': 1,
 		},
 	},
+	'toolbarOpen': {
+		'display': 'block',
+	},
 	'menuButton': {
-		'marginRight': theme.spacing(2),
+		[theme.breakpoints.up('md')]: {
+			'display': 'none',
+			'marginRight': theme.spacing(2),
+		},
 	},
 	'navLink': {
 		[theme.breakpoints.up('md')]: {
@@ -38,6 +47,7 @@ const useStyles = makeStyles((theme) => ({
 const Navbar: React.FunctionComponent = () => {
 	const location = useLocation();
 	const classes = useStyles();
+	const [menuOpen, setMenuOpen] = useState(false);
 
 	const menuItems = [
 		{ 'text': 'Home', 'path': '/' },
@@ -45,6 +55,10 @@ const Navbar: React.FunctionComponent = () => {
 		{ 'text': 'Simple state', 'path': '/simple-state' },
 		{ 'text': 'Environment variable', 'path': '/env-var' },
 	];
+
+	toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
 
 	// the NavLink component adds "active" class but this is not compatible with Material UI. By using Link and adding "active" with makeStyles, we keep all navbar styling in this file
 	const makeNavLink = ({
@@ -60,7 +74,12 @@ const Navbar: React.FunctionComponent = () => {
 	return (
 		<div className={classes.root}>
 			<AppBar position="static">
-				<Toolbar className={classes.toolbar}>
+				<Box display="flex" flexDirection="row-reverse">
+					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => toggleMenu()}>
+						<MenuIcon />
+					</IconButton>
+				</Box>
+				<Toolbar className={`${classes.toolbar} ${menuOpen ? classes.toolbarOpen : ''}`}>
 					{menuItems.map((item) => makeNavLink(item))}
 				</Toolbar>
 			</AppBar>
